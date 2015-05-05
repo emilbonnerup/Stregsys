dc65c86a2ae0cf0d5bf966bfaf8bfd4af49a4659
emilbonnerup <emilbonnerup@me.com>
2015-05-05 14:19:57 +0200

    Start

---
+++ Stregsystem/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stregsystem
+{
+    class Product
+    {
+        int productId;
+        string name;
+        double price;
+        bool active;
+        bool canBeBoughtOnCredit;
+
+    }
+}
---
+++ Stregsystem/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stregsystem
+{
+    class User
+    {
+        int userId;
+        string firstName;
+        string lastName;
+        string userName;
+        string email;
+        double balance;
+    }
+}
